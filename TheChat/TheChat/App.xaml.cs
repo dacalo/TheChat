c65f7dcd86ffddaa878014bc3e9b295d46dc4685
David Chavez Lopez <david.chavez.lopez1@outlook.com>
2020-06-29 09:30:58 -0500

    Create project mobile, install package FreshMVVM, Material Design and copy images

---
+++ TheChat/TheChat/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using FreshMvvm;
+using System;
+using TheChat.Core.Services;
+using TheChat.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TheChat
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            ConfigureContainer();
+
+            var loginPage = FreshPageModelResolver.ResolvePageModel<LoginViewModel>();
+
+            var navPage = new FreshNavigationContainer(loginPage);
+
+            MainPage = navPage;
+        }
+
+        private void ConfigureContainer()
+        {
+            FreshIOC.Container.Register<IChatService, ChatService>();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ TheChat/TheChat/ViewModels/LoginViewModel.cs
@@ -0,0 +1,11 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheChat.ViewModels
+{
+    public class LoginViewModel : FreshBasePageModel
+    {
+    }
+}
